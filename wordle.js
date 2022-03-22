52b509054ba454ca45268098f7804b161db19e8b
Kimberley Rogers <kimberleyrogers@live.co.uk>
2022-03-22 20:50:12 +1100

    Each valid guess prints on a new row

--- wordle.js
+++ wordle.js
@@ -1,65 +1,49 @@
 //array of possible words - small to begin with
-const wordleArray = ['hello', 'jimin', 'jhope', 'kimbo'];
+const wordleArray = ['HELLO', 'RESET', 'BEACH', 'SHARK'];
 //randomly select a word from the array for today's word //split today's wordle into an array
 const todaysWordle = wordleArray[Math.floor(Math.random() * wordleArray.length)]
 const todaysWordleArray = todaysWordle.split("")
 console.log("today's Wordle array: " + todaysWordleArray)
 
-//link from additional JS file next
-// const validWords = ['reset', 'going', 'beach', 'hello', 'jimin', 'jhope', 'kimbo'];
-
-//take a first guess from the user //split the user prompted word into an array
-// const firstUserGuess = prompt("What is your guess?")
-// const firstUserGuessArray = firstUserGuess.split("")
-// console.log("user first guess array is: " + firstUserGuessArray)
-
-//get value from input box using add event listener
-//before - prompt happened first and everything on the page followed
-//now you need the entry of a word to be the trigger for everything else
 const guessLocation = document.getElementById("guess-box")
 let levelCounter = 1
-let firstUserGuess = ""
-let firstUserGuessArray = ""
+let userGuess = ""
+let userGuessArray = ""
+
 guessLocation.addEventListener('keypress', function(e) {
     //when enter key is pressed, take value as guess
-    
     if(e.key == 'Enter') {
-        firstUserGuess = guessLocation.value.toUpperCase()
-        console.log("firstUserGuess is " + firstUserGuess)
+        userGuess = guessLocation.value.toUpperCase()
+        console.log("firstUserGuess is " + userGuess)
         //check word is valid
-        if(validWords.includes(firstUserGuess)) {
+        if(validWords.includes(userGuess)) {
             console.log("guessLocation.value is " + guessLocation.value)
-            console.log("firstUserGuess is " + firstUserGuess)
-            const firstUserGuessArray = firstUserGuess.split("")
-            console.log("firstUserGuessArray is " + firstUserGuessArray)
-            checkColours(firstUserGuessArray)
+            const userGuessArray = userGuess.split("")
+            console.log("firstUserGuessArray is " + userGuessArray)
+            checkColours(userGuessArray, levelCounter)
             //word is a valid guess so increase level counter
             levelCounter++
             console.log(levelCounter)
             guessLocation.value = ""
         } else {
+            //MAKE THIS APPEAR IN THE HTML
             console.log("Word wasn't valid")
+            guessLocation.value = ""
         }
     }
-    
 })
 
-
-
-
-//function for each valid guess - checks each letter against the wordle and displays colour
-function checkColours(firstUserGuessArray) {
+//function for each valid guess - checks each letter against the wordle, adds to box and displays colour
+function checkColours(userGuessArray, levelCounter) {
+    console.log(userGuessArray, levelCounter)
     let letterPositionCounter = 0
-    //check if the letter+position matches, turn letter green
-    for (const guessLetter of firstUserGuessArray) {
-        //update the h1 for each letter //could be updating anything - a p in a box in grid/flex 
-        const location = document.getElementById("letter-" + letterPositionCounter)
+    for (const guessLetter of userGuessArray) {
+        const location = document.getElementById("guess-" + levelCounter + "-letter-" + letterPositionCounter)
         location.innerHTML = guessLetter
-        //update the class for each letter that is a green match
-        if (firstUserGuessArray[letterPositionCounter] === todaysWordleArray[letterPositionCounter]) {
+        if (userGuessArray[letterPositionCounter] === todaysWordleArray[letterPositionCounter]) {
             location.classList.add('match')
         } else if (todaysWordle.includes(guessLetter)) {
-            //if you can - try to reoslve issue that Kenni identified
+            //if you can - try to resolve issue that Kenni identified
             location.classList.add('partial-match')
         } else {
             location.classList.add('no-match')
@@ -68,13 +52,12 @@ function checkColours(firstUserGuessArray) {
     }
     //check if guessing was winning guess 
     if(document.querySelectorAll('.match').length == 5){
-
         console.log("You won!")
     }
 }
-//if all green = you've won
 
 
+//if still broken just use classname = instead of classList.add
 //css for pattern on page
 
 //counter for each level attempted
